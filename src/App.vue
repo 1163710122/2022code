6631dc3794c420bcb9d97e7ba897dd18cc1ab1ca
1163710122 <18945590698@163.com>
2022-02-26 21:12:53 +0800

    start

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img>
+    <div>这是一个新项目</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/homepage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>hello world</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "homepage",
+  data() {
+    return {
+      message: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
